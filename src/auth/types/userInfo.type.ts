0a21b31fef0af1058cbe47d7582cab09178493b4
Kimcheolhui <cjfgml0306@naver.com>
2025-06-08 03:01:43 +0900

    feat: user dto, type 수정

--- src/auth/types/userInfo.type.ts
+++ src/auth/types/userInfo.type.ts
@@ -2,5 +2,6 @@ export type UserInfo = {
   uuid: string;
   kakaoId: string;
   nickname: string;
+  profileImageUrl?: string;
   createdAt: Date;
 };
--- src/user/dto/res/userInfo.dto.ts
+++ src/user/dto/res/userInfo.dto.ts
@@ -23,6 +23,15 @@ export class UserInfoDto {
   nickname: string;
 
   @ApiProperty({
+    type: String,
+    description: "User's profile image signed URL",
+    example:
+      'https://your-bucket.s3.region.amazonaws.com/production/profile/1234567890-abcdef.webp?signed-params',
+    required: false,
+  })
+  profileImageUrl?: string;
+
+  @ApiProperty({
     type: Date,
     description: 'Account created Date',
     example: '2025-01-30T15:34:28.284Z',
