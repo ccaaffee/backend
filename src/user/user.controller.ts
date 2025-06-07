dc821ce447a0d250d591ed47c8532068045163a1
Kimcheolhui <cjfgml0306@naver.com>
2025-06-08 01:26:15 +0900

    feat: 닉네임 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, Body, Patch, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/auth/jwt.auth.strategy';
 import { GetUser } from './decorator/get-user.decorator';
@@ -8,10 +8,13 @@ import {
   ApiInternalServerErrorResponse,
   ApiOkResponse,
   ApiOperation,
+  ApiQuery,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { UserInfoDto } from './dto/res/userInfo.dto';
+import { UpdateNicknameDto } from './dto/req/updateNickname.dto';
+import { NicknameDuplicateCheckDto } from './dto/res/nicknameDuplicateCheck.dto';
 
 @ApiTags('User')
 @Controller('user')
@@ -34,5 +37,50 @@ export class UserController {
   @UseGuards(JwtAuthGuard)
   async getProfile(@GetUser() user: UserInfo): Promise<UserInfo> {
     return user;
+  }
+
+  @ApiOperation({
+    summary: 'check nickname duplicate',
+  })
+  @ApiQuery({
+    name: 'nickname',
+    type: String,
+    description: 'Nickname to check',
+    required: true,
+  })
+  @ApiOkResponse({
+    type: NicknameDuplicateCheckDto,
+    description: 'Return whether nickname is duplicate or not',
+  })
+  @Get('nickname/check')
+  async checkNicknameDuplicate(
+    @Query('nickname') nickname: string,
+  ): Promise<NicknameDuplicateCheckDto> {
+    const isDuplicate = await this.userService.checkNicknameDuplicate(nickname);
+    return { isDuplicate };
+  }
+
+  @ApiOperation({
+    summary: 'update my nickname',
+  })
+  @ApiOkResponse({
+    type: UserInfoDto,
+    description: 'Return updated profile',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('JWT')
+  @Patch('nickname')
+  @UseGuards(JwtAuthGuard)
+  async updateNickname(
+    @GetUser() user: UserInfo,
+    @Body() updateNicknameDto: UpdateNicknameDto,
+  ): Promise<UserInfo> {
+    return this.userService.updateNickname(
+      user.uuid,
+      updateNicknameDto.nickname,
+    );
   }
 }
